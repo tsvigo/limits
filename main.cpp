35c2595b90582f71736298d3dffcc392b66af3a4
Viktor Tsvigo <11_70@bk.ru>
2024-06-05 22:39:24 +0300

    Описание коммита

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <stdint.h>
 #include <fstream>
 #include <iostream>
+using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     std::cout << ULLONG_MAX << std::endl;
+      std::cout << LLONG_MAX << LLONG_MAX<<std::endl;
     return a.exec();
 }
